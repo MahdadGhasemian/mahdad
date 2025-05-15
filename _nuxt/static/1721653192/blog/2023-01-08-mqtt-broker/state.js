72b69618b63f1a3aa64d2967e2dac6169aba5d17
MahdadGhasemian <MahdadGhasemian@users.noreply.github.com>
2025-05-15 08:27:40 +0000

    deploy: 8bcc9ae488ccdc9e2d733494082b815f1aa63cda

--- _nuxt/static/1721653192/blog/2023-01-08-mqtt-broker/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1721653192",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2023-01-08-mqtt-broker",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9d1c7a1"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null));
