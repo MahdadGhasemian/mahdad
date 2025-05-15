61e7a16d328bb4d48fa0383267ef54397aee04b6
MahdadGhasemian <MahdadGhasemian@users.noreply.github.com>
2025-05-15 08:37:31 +0000

    deploy: d9e184a19470f519eb49ce22e2afd29f2767f5fe

---
+++ _nuxt/static/1747298250/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1747298250",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39c5b40c"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null,"\u002F"));
